fe51d9c138dbb05d05a929f5be2f91d4e7059bd8
Jeremy Reist <jeremyreist@gmail.com>
2022-10-21 12:24:34 -0400

    Added dists folder.

--- dist/test.js
+++
@@ -1,5 +0,0 @@
-var netflix;
-const videoPlayer = netflix.appContext.state.playerApp.getAPI().videoPlayer;
-const player = videoPlayer.getVideoPlayerBySessionId(videoPlayer.getAllPlayerSessionIds()[0]);
-const currentTime = player.getCurrentTime();
-console.log(currentTime);
--- dist/test.ts
+++
@@ -1,5 +0,0 @@
-declare var netflix: any
-const videoPlayer = netflix.appContext.state.playerApp.getAPI().videoPlayer;
-const player = videoPlayer.getVideoPlayerBySessionId(videoPlayer.getAllPlayerSessionIds()[0]);
-const currentTime = player.getCurrentTime();
-console.log(currentTime);
